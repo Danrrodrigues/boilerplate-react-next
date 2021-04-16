1fb6da04c7cc97bebeff5b6e7451411dfbd8a9ee
Danilo Note <danrrodrigues@gmail.com>
2021-04-15 22:25:42 -0300

    Adicionado compoente de button, text e theme

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,10 +1,28 @@
+import { ThemeProvider } from 'styled-components'
 import GlobalStyles from '../src/styles/global'
+import theme from '../src/styles/theme'
+
+export const parameters = {
+  backgrounds: {
+    default: 'breact-next-light',
+    values: [
+      {
+        name: 'breact-next-light',
+        value: theme.colors.white
+      },
+      {
+        name: 'breact-next-dark',
+        value: theme.colors.mainBg
+      }
+    ]
+  }
+}
 
 export const decorators = [
   (Story) => (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalStyles />
       <Story />
-    </>
+    </ThemeProvider>
   )
 ]
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,32 @@
+import { useState } from 'react'
 import Main from 'components/Main'
+import Button from 'components/Button'
+import TextField from 'components/TextField'
+
+import { Email, Lock, ErrorOutline } from '@styled-icons/material-outlined'
+
+import { FormLink, FormWrapper, FormLoading, FormError } from 'components/Form'
 
 export default function Home() {
-  return <Main />
+  const [formError, setFormError] = useState('')
+  const handleSubmit = async (event: React.FormEvent) => {}
+  return (
+    <>
+      <Main />
+      <FormWrapper>
+        {!!formError && (
+          <FormError>
+            <ErrorOutline /> {formError}
+          </FormError>
+        )}
+        <form onSubmit={handleSubmit}>
+          <TextField name="email" placeholder="Texto teste" />
+          <Button type="submit" size="large" fullWidth>
+            <span>Send email</span>
+          </Button>
+          <FormLink>Don’t have an account? </FormLink>
+        </form>
+      </FormWrapper>
+    </>
+  )
 }
